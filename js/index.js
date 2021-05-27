d45139c37b381df2344b0941b5a44cb534c665a5
charles-ekube <charleswakili797@gmail.com>
2021-05-27 15:28:09 +0100

    updated styylesheets

--- js/index.js
+++ js/index.js
@@ -1,20 +1,58 @@
 // document.getElementById('search-label').onclick =  () => {
 //     .style.background='yellow';
 // }
-document.getElementById("search-label").addEventListener("click", function() {
-    document.getElementById("search-label").style.width = '400px';
-  });
+// document.getElementById("search-label").addEventListener("click", function() {
+//     document.getElementById("search-label").style.width = '400px';
+//   });
 
 
 
-const mobileMenuBtn = document.getElementById("mobile-menu-btn");
-const mobileMenu = document.getElementById('mobile-menu');
+// const mobileMenuBtn = document.getElementById("mobile-menu-btn");
+// const mobileMenu = document.getElementById('mobile-menu');
 
-mobileMenuBtn.addEventListener('click',  () => {
-    if(mobileMenu.style.display = "none"){
-        mobileMenu.style.display = "block";
+// mobileMenuBtn.addEventListener('click',  () => {
+//     if(mobileMenu.style.display = "none"){
+//         mobileMenu.style.display = "block";
+//     }
+//     else if(mobileMenu.style.display = "block") {
+//         mobileMenu.style.display="none";
+//     }
+// })
+
+// mobile nav toggle function
+const mobileNavToggle = document.getElementById('mobile-nav-toggle');
+const mobileNavDropdown = document.getElementById('mobile-nav-dropdown');
+
+mobileNavToggle.onclick = () => {
+    
+        if(mobileNavDropdown.style.display === 'none'){
+    
+            mobileNavDropdown.style.display = 'block';
+        }
+        else {
+            mobileNavDropdown.style.display = 'none';
+        }
     }
-    else if(mobileMenu.style.display = "block") {
-        mobileMenu.style.display="none";
+
+
+// tabs function
+const toggleTabs = (e, tabName) => {
+    
+    const tabsContent = document.getElementsByClassName('tabs-content-item');
+    const tabLinks = document.getElementsByClassName('tabs-list-item');
+
+    for (var i = 0; i < tabsContent.length; i++) {
+        tabsContent[i].style.display = 'none';
     }
-})
+    
+    for (var i = 0; i < tabLinks.length; i++) {
+        tabLinks[i].className = tabLinks[i].className.replace('active', '');
+    }
+
+    document.getElementById(tabName).style.display = 'block';
+    e.currentTarget.className += 'active';
+    
+
+    
+
+} 
